10ebe9ae66af1b5c39654cf9a3383dc221e66575
Ian Thomas <ianthomas23@gmail.com>
2024-08-13 12:17:45 +0100

    Move WebWorker code to cockle

--- src/defs.ts
+++ src/defs.ts
@@ -1,37 +1,5 @@
-import { ProxyMarked, Remote } from 'comlink'
-
 export namespace IPlayground {
   export interface IOptions {
     targetDiv: HTMLElement
   }
-
-  export type IOutputCallback = (output: string) => void
-  export type IEnableBufferedStdinCallback = (enable: boolean) => void
 }
-
-export interface IWorkerPlayground {
-  input(text: string): Promise<void>;
-  setSize(rows: number, columns: number): Promise<void>;
-  start(): Promise<void>;
-}
-
-export namespace IWorkerPlayground {
-  export interface IOptions {
-    sharedArrayBuffer: SharedArrayBuffer
-  }
-}
-
-export namespace IRemote {
-  export type OutputCallback = IPlayground.IOutputCallback & ProxyMarked
-  export type EnableBufferedStdinCallback = IPlayground.IEnableBufferedStdinCallback & ProxyMarked
-}
-
-export interface IRemote extends IWorkerPlayground {
-  initialize(options: IWorkerPlayground.IOptions): Promise<void>
-  registerCallbacks(
-    outputCallback: IRemote.OutputCallback,
-    enableBufferedStdinCallback: IRemote.EnableBufferedStdinCallback,
-  ): void
-}
-
-export type IRemoteWorkerPlayground = Remote<IRemote>
--- src/playground.ts
+++ src/playground.ts
@@ -1,8 +1,7 @@
+import { Shell } from '@jupyterlite/cockle'
 import { FitAddon } from '@xterm/addon-fit'
 import { Terminal } from '@xterm/xterm'
-import { proxy, wrap } from 'comlink'
-import { MainBufferedStdin } from './buffered_stdin'
-import { IPlayground, IRemoteWorkerPlayground } from './defs'
+import { IPlayground } from './defs'
 
 export class Playground {
   constructor(options: IPlayground.IOptions) {
@@ -21,8 +20,15 @@ export class Playground {
     this._fitAddon = new FitAddon()
     this._term.loadAddon(this._fitAddon)
 
-    this._bufferedStdin = new MainBufferedStdin()
-    this._initWorker()
+    this._shell = new Shell({
+      outputCallback: this.outputCallback.bind(this),
+      initialDirectories: ['dir'],
+      initialFiles: {
+        'file.txt': 'This is the contents of the file',
+        'other.txt': 'Some other file\nSecond line',
+        'months.txt': 'January\nFebruary\nMarch\nApril\nMay\nJune\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember\n'
+      }
+    })
   }
 
   async start(): Promise<void> {
@@ -34,55 +40,24 @@ export class Playground {
     })
 
     this._term!.open(this._options!.targetDiv)
-    await this._remote!.start()
+    await this._shell.start()
     resizeObserver.observe(this._options!.targetDiv)
   }
 
   async onKey(arg: any): Promise<void> {
-    const char = arg.key as string
-    if (this._bufferedStdin.enabled) {
-      await this._bufferedStdin.push(char)
-    } else {
-      await this._remote!.input(char)
-    }
+    await this._shell.input(arg.key)
   }
 
   async onResize(arg: any): Promise<void> {
-    await this._remote!.setSize(arg.rows, arg.cols)
+    await this._shell.setSize(arg.rows, arg.cols)
   }
 
-  private async _initWorker() {
-    this._worker = new Worker(new URL('./worker_playground.ts', import.meta.url), {
-      type: 'module'
-    })
-
-    this._remote = wrap(this._worker)
-    const { sharedArrayBuffer } = this._bufferedStdin
-    await this._remote.initialize({ sharedArrayBuffer })
-    this._remote.registerCallbacks(
-      proxy(this.outputCallback.bind(this)),
-      proxy(this.enableBufferedStdinCallback.bind(this)),
-    )
-
-    this._bufferedStdin.registerSendStdinNow(this._remote.input)
-  }
-
-  private async enableBufferedStdinCallback(enable: boolean) {
-    if (enable) {
-      await this._bufferedStdin.enable()
-    } else {
-      await this._bufferedStdin.disable()
-    }
-  }
-
-  private outputCallback(text: string): void {
+  private async outputCallback(text: string): Promise<void> {
     this._term!.write(text)
   }
 
   private _options: IPlayground.IOptions
-  private _worker?: Worker
-  private _remote?: IRemoteWorkerPlayground
   private _term: Terminal
   private _fitAddon: FitAddon
-  private _bufferedStdin: MainBufferedStdin
+  private _shell: Shell
 }
